280776c6cb20c4eeeafc6f62a2ff9fde55f2f57a
suvadeepghoshal <ghoshalsuvadeep594e@gmail.com>
2022-11-13 12:15:29 +0530

    footer designed

---
+++ src/util/data.js
@@ -0,0 +1,19 @@
+export const data = () => {
+  [
+    {
+      id: Math.floor(Math.random() * 100),
+      href: 'https://www.instagram.com/suvadeepghoshal',
+      dataIcon: 'instagram'
+    },
+    {
+      id: Math.floor(Math.random() * 100),
+      href: 'https://www.linkedin.com/in/suvadeep-ghoshal-778426197/',
+      dataIcon: 'linkedin-in'
+    },
+    {
+      id: Math.floor(Math.random() * 100),
+      href: 'https://github.com/suvadeepghoshal',
+      dataIcon: 'github'
+    }
+  ];
+};
