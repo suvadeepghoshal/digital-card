dca671d5f38e8e2a6ec05cede7be2621b6cee3f8
suvadeepghoshal <ghoshalsuvadeep594e@gmail.com>
2022-11-13 19:16:31 +0530

    removed data

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 import './App.css';
 import { Banner } from './components/Banner/Banner';
 import { Footer } from './components/Footer/Footer';
-import { data } from './util/data';
 
 function App() {
   return (
     <div className="bg-slate-100 p-8 dark:bg-slate-800">
       <Banner />
-      <Footer {...data} />
+      <Footer />
     </div>
   );
 }
